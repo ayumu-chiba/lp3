1055bb0f70456c85503abb3a79055180ae887549
ayumu-chiba <jmw32813281@gmail.com>
2026-01-26 00:31:36 +0900

    10回目　PC表示でカルーセルUIが動作しない　修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,48 +1,22 @@
 $(function () {
-  const $slider = $('.cover-slick');
+  $('.cover-slick').slick({
+    infinite: true,
+    arrows: true,
+    dots: true,
 
-  function toggleSlick() {
-    if (window.matchMedia('(max-width: 767.8px)').matches) {
-      if (!$slider.hasClass('slick-initialized')) {
-        $slider.slick({
-          infinite: true,
-          dots: true,
-          arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
           slidesToShow: 1,
+          slidesToScroll: 1,
           centerMode: true,
           centerPadding: '2rem'
-        });
+        }
       }
-    } else {
-      if ($slider.hasClass('slick-initialized')) {
-        $slider.slick('unslick');
-      }
-    }
-  }
-
-  toggleSlick();
-  $(window).on('resize', toggleSlick);
-});
-
-
-
-const $slider = $('.cover-slick');
-$slider.slick({
-  infinite: true,
-  slidesToShow: 1,
-  dots: false
-});
-const $dots = $('.cover-slick-dots .dot');
-
-$dots.each(function(index){
-  $(this).on('click', function(){
-    $slider.slick('slickGoTo', index);
-    $dots.removeClass('dot--active');
-    $(this).addClass('dot--active');
+    ]
   });
-});
-
-$slider.on('afterChange', function(event, slick, currentSlide){
-  $dots.removeClass('dot--active');
-  $dots.eq(currentSlide).addClass('dot--active');
 });
